6cbbb36f7f5fab3f5728b833d7d6f64c8dd7c19d
Amadmziri1 <amadmziri1@icloud.com>
2023-12-28 23:28:16 +0300

    first

---
+++ js.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function () {
+
+  
+// not able right click my site 
+      document.addEventListener("contextmenu", (event) =>
+        event.preventDefault()
+      );
+
+
+
+const navLinkEls = document.querySelectorAll("nav ul li a"); // Use correct class selector syntax
+const windowPathname = window.location.pathname;
+
+navLinkEls.forEach((navLinkEl) => {
+  const navLinkPathname = new URL(navLinkEl.href).pathname; // Fix syntax for creating URL object
+
+  if (windowPathname === navLinkPathname || windowPathname === "/index.html" && navLinkPathname === '/') {
+    navLinkEl.classList.add("active");
+  }
+});
+
+
+
+  $("header #list-icon").click(function () {
+    // Toggle the 'show' class on the 'nav ul'
+    $("nav ul").addClass("show");
+    $("#close-icon").css("display", "block");
+  });
+
+  $("header #close-icon").click(function () {
+    // Toggle the 'show' class on the 'nav ul'
+    $("nav ul").removeClass("show");
+    $("#close-icon").css("display", "none");
+  });
+
+  // Function to perform actions when the window is resized
+  function checkWindowSize() {
+    // Get the current window width
+    var windowWidth = $(window).width();
+
+    // Check if the window width is more than 700 pixels
+    if (windowWidth > 700) {
+      $("#close-icon").css("display", "none");
+      $("nav ul").removeClass("show");
+    }
+  }
+
+  // Call the function whenever the window is resized
+  $(window).resize(function () {
+    checkWindowSize();
+  });
+});
